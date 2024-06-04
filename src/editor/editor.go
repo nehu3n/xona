dbd15cc4b288f1851ab22b691ac53d10e5128386
nehu3n <developer.nh3@gmail.com>
2024-06-04 11:21:40 -0300

    ✨ feat(editor): Manual scrolling.
    The ability to manually scroll with the mouse wheel has been added.

--- src/editor/editor.go
+++ src/editor/editor.go
@@ -19,6 +19,9 @@ func Editor(filePath string) {
 	}
 	defer screen.Fini()
 
+	screen.EnableMouse()
+	var mouseScrollActive bool = true
+
 	buffer := NewBuffer()
 
 	if filePath != "" {
@@ -50,10 +53,12 @@ func Editor(filePath string) {
 		_, screenHeight := screen.Size()
 
 		cursorX, cursorY := buffer.GetCursor()
-		if cursorY < buffer.viewTop {
-			buffer.viewTop = cursorY
-		} else if cursorY >= buffer.viewTop+screenHeight {
-			buffer.viewTop = cursorY - screenHeight + 1
+		if !mouseScrollActive {
+			if cursorY < buffer.viewTop {
+				buffer.viewTop = cursorY
+			} else if cursorY >= buffer.viewTop+screenHeight {
+				buffer.viewTop = cursorY - screenHeight + 1
+			}
 		}
 
 		for y := 0; y < screenHeight; y++ {
@@ -78,30 +83,72 @@ func Editor(filePath string) {
 		screen.Show()
 	}
 
+	adjustViewTop := func() {
+		cursorY, _ := buffer.GetCursor()
+		_, screenHeight := screen.Size()
+		if cursorY < buffer.viewTop {
+			buffer.viewTop = cursorY
+		} else if cursorY >= buffer.viewTop+screenHeight {
+			buffer.viewTop = cursorY - screenHeight + 1
+		} else if cursorY >= buffer.viewTop+screenHeight-1 {
+			buffer.viewTop++
+		}
+	}
+
+	handleKey := func(key *tcell.EventKey) {
+		mouseScrollActive = false
+
+		switch key.Key() {
+		case tcell.KeyCtrlC:
+			quit()
+			return
+		case tcell.KeyEnter:
+			buffer.InsertNewline()
+			adjustViewTop()
+		case tcell.KeyRune:
+			buffer.Insert(key.Rune())
+			adjustViewTop()
+		case tcell.KeyBackspace, tcell.KeyBackspace2:
+			buffer.Delete()
+			adjustViewTop()
+		case tcell.KeyLeft:
+			buffer.MoveCursor(-1, 0)
+			adjustViewTop()
+		case tcell.KeyRight:
+			buffer.MoveCursor(1, 0)
+			adjustViewTop()
+		case tcell.KeyUp:
+			buffer.MoveCursor(0, -1)
+			adjustViewTop()
+		case tcell.KeyDown:
+			buffer.MoveCursor(0, 1)
+			adjustViewTop()
+		}
+	}
+
+	handleMouse := func(mouse *tcell.EventMouse) {
+		_, screenHeight := screen.Size()
+		if mouse.Buttons() == tcell.WheelDown {
+			mouseScrollActive = true
+			if buffer.viewTop < len(buffer.content)-screenHeight {
+				buffer.viewTop++
+			}
+		} else if mouse.Buttons() == tcell.WheelUp {
+			mouseScrollActive = true
+			if buffer.viewTop > 0 {
+				buffer.viewTop--
+			}
+		}
+	}
+
 	for {
 		draw()
 		ev := screen.PollEvent()
 		switch ev := ev.(type) {
 		case *tcell.EventKey:
-			switch ev.Key() {
-			case tcell.KeyCtrlC:
-				quit()
-				return
-			case tcell.KeyEnter:
-				buffer.InsertNewline()
-			case tcell.KeyRune:
-				buffer.Insert(ev.Rune())
-			case tcell.KeyBackspace, tcell.KeyBackspace2:
-				buffer.Delete()
-			case tcell.KeyLeft:
-				buffer.MoveCursor(-1, 0)
-			case tcell.KeyRight:
-				buffer.MoveCursor(1, 0)
-			case tcell.KeyUp:
-				buffer.MoveCursor(0, -1)
-			case tcell.KeyDown:
-				buffer.MoveCursor(0, 1)
-			}
+			handleKey(ev)
+		case *tcell.EventMouse:
+			handleMouse(ev)
 		case *tcell.EventResize:
 			screen.Sync()
 		}
