8af3cfb4e44bb2d2d08951c9ba0b5a6686827833
nehu3n <developer.nh3@gmail.com>
2024-06-16 19:06:49 -0300

    ♻️ refactor(editor): Code block changed from file.

--- src/editor/editor.go
+++ src/editor/editor.go
@@ -66,31 +66,6 @@ func NewEditor() *Editor {
 	}
 }
 
-func (e *Editor) openNewWindow() {
-	if len(e.windows) == 4 {
-		return
-	}
-
-	windowScreen, err := tcell.NewScreen()
-	if err != nil {
-		log.Fatalf("Failed to create screen: %v", err)
-	}
-
-	e.windows = append(e.windows, &Window{buffer: NewBuffer(), screen: windowScreen})
-	currentWindowIndex = len(e.windows) - 1
-
-	if err := windowScreen.Init(); err != nil {
-		log.Fatalf("Failed to initialize new window screen: %v", err)
-	}
-}
-func (e *Editor) switchWindow() {
-	numWindows := len(e.windows)
-	if numWindows > 1 {
-		currentWindowIndex = (currentWindowIndex + 1) % numWindows
-	}
-
-}
-
 func (e *Editor) Run(filePath string) {
 	currentWindowIndex = 0
 	var screen tcell.Screen = e.windows[currentWindowIndex].screen
